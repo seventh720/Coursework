4f81ba4289e6312ef22e403f53bb0055a9c498ba
JUNYI LU <j.lu.22@abdn.ac.uk>
2023-11-06 11:31:57 +0000

    Calculator

---
+++ Practical4/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator{
+    //Adds two integers and returns the result.
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    //Subtracts the second integer from the first integer and returns the result.
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //Multiplies two integers and returns the result.
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
